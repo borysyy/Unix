c398f450c8a6c8906467706f0faf4de8fe949227
Slava Borysyuk <yaroslavborysyuk1@gmail.com>
2025-02-25 15:30:55 -0500

    Separated things into functions and create a file for the conversation

--- HW#2/hw2.py
+++ HW#2/hw2.py
@@ -1,19 +1,36 @@
 import requests
 import json
+from datetime import datetime
 from pprint import pprint
+
+def call_api(model, ip):
+    data = {
+        "model": model,
+        "messages": chat_history,
+        "stream": False
+    }
     
+    url = f"http://{ip}:11434/api/chat"
+    response = requests.post(url, json=data)
+    
+    return response
+
+
+def print_write_file(r):
+    global f
+    
+    print(r)
+    
+    f = open(f.name, "a")
+    f.write(r)
+    f.close()
+
+
 def first_ai(chat_history):
     global first_ai_model, first_ai_ip
     
-    data = {
-            "model": first_ai_model,
-            "messages": chat_history,
-            "stream": False
-    }
-    
-    url = f"http://{first_ai_ip}:11434/api/chat"
-    response = requests.post(url, json=data)
-    
+    response = call_api(first_ai_model, first_ai_ip)
+
     if response.status_code == 200:
         
         json_data = response.json()
@@ -23,25 +40,21 @@ def first_ai(chat_history):
         first_ai_response["role"] = "user"
         
         chat_history.append(first_ai_response)
+        
+        r = f"\nFIRST AI RESPONSE {first_ai_model} - {first_ai_response['content']}"
                 
-        print(f"\nREMOTE RESPONSE - {first_ai_response['content']}")
+        print_write_file(r)        
         
         second_ai(chat_history)
         
     else:
-        print(f"Error: {response.status_code}")
+        print(f"Error FIRST AI: {response.status_code}")
+
 
 def second_ai(chat_history):
-    global second_ai_ip, second_ai_ip
+    global second_ai_model, second_ai_ip
     
-    data = {
-        "model": second_ai_ip,
-        "messages": chat_history,
-        "stream": False
-    }
-    
-    url = f"http://{second_ai_ip}:11434/api/chat"
-    response = requests.post(url, json=data)
+    response = call_api(second_ai_model, second_ai_ip)
     
     if response.status_code == 200:
         
@@ -52,27 +65,36 @@ def second_ai(chat_history):
         second_ai_response["role"] = "user"
             
         chat_history.append(second_ai_response)
-
-        print(f"\nLOCAL RESPONSE - {second_ai_response['content']}")
+        
+        r = f"\nSECOND AI RESPONSE {second_ai_model} - {second_ai_response['content']}"
+        
+        print_write_file(r)        
 
         first_ai(chat_history)
     
     else:
-        print(f"Error: {response.status_code}")
+        print(f"SECOND AI Error: {response.status_code}")
     
     
 
 if __name__ == "__main__":
     chat_history = []
     
+    print("Make sure the specified IP has the specified Model installed\n")
+    
     first_ai_ip = str(input("First IP: "))
     first_ai_model = str(input("First Model: "))
     
     second_ai_ip = str(input("Second IP: "))
-    second_ai_ip = str(input("Second Model: "))
+    second_ai_model = str(input("Second Model: "))
     
     initial_message = str(input("Initial message: "))
     chat_history.append({"role": "user", "content": initial_message})
     
+    d = datetime.now()
+    
+    f = open(f"conversation-{d}", "x")
+    
     first_ai(chat_history)
+
     
